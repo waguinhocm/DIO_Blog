c219fa718265c9ecdb0c263672540736ae2bc7bc
Wagner Muniz Camargo <camargowm@saude.sc.gov.br>
2026-06-09 09:38:42 -0300

    introduzindo sqlalchemy

--- src/app.py
+++ src/app.py
@@ -1,18 +1,64 @@
-from flask import Flask
+
+import click
+from datetime import datetime
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import os
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    ubody: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
+
+@click.command('init-db')
+def init_db_command():
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE='diobank.sqlite',
+        SQLALCHEMY_DATABASE_URI='sqlite:///dio_bank.sqlite',
     )
-    
+
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
-    from . import db
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.cli.add_command(init_db_command)
     db.init_app(app)
-    
+
     return app
